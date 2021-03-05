ebccd76f5c591d91f6b7c9958774ff8075d89f24
egebaserr <80101908+egebaserr@users.noreply.github.com>
2021-03-05 23:05:34 +0300

    Update curl-request.py

--- curl-request.py
+++ curl-request.py
@@ -1,3 +1,8 @@
-curl JENKINS_URL/job/JOB_NAME/buildWithParameters \
-      --user USER:TOKEN \
-      --data id=123 --data verbosity=high
+import requests
+
+data = {
+  'id': '123',
+  'verbosity': 'high'
+}
+
+response = requests.post('http://JENKINS_URL/job/JOB_NAME/buildWithParameters', data=data, auth=('USER', 'TOKEN'))
